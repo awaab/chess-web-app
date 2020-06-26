9d1768377f52ce0faf4f4919ce3bc6e288b80df8
awaab <awaaab10@gmail.com>
2020-06-26 17:43:52 +0400

    remove build from gitignore

---
+++ react-chess-app/build/precache-manifest.0e2e5c3b0bd8340d3d022b8e0e23c94f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1afe351e52e979a5a77401bf9b7571b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da5ee207e387dedb082c",
+    "url": "/static/css/main.61c96360.chunk.css"
+  },
+  {
+    "revision": "d95eb2fa2dff20d4d182",
+    "url": "/static/js/2.37bb5f38.chunk.js"
+  },
+  {
+    "revision": "da5ee207e387dedb082c",
+    "url": "/static/js/main.d17e37cf.chunk.js"
+  },
+  {
+    "revision": "e6c29f1936c9220db6e1",
+    "url": "/static/js/runtime-main.00867912.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
