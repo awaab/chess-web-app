abdf512939e8f32bb2253d64e89016b72e57f750
awaab <awaaab10@gmail.com>
2020-06-27 09:23:38 +0400

    debug mode

--- chess_site/settings.py
+++ chess_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '34wnhoj6bncv8yhug+3g7&$-z@@-of!$+j)%dil68oevyf0a7b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 
 ALLOWED_HOSTS = ['exchesswebapp.herokuapp.com', 'localhost']
 
